849a81bfa639d32eaf7c4f2f71bed62c950c4483
Md Ahanaf Islam <ahanafislam@gmail.com>
2022-02-16 03:17:39 +0600

    Add Numberpad functionality

--- Milestone_6/Module_28/pin-matcher/app.js
+++ Milestone_6/Module_28/pin-matcher/app.js
@@ -7,7 +7,29 @@ function generateRandomNum(){
     return random;
 }
 
+// Pin Generate Event Listener
 document.getElementById('pin-generate-btn').addEventListener('click', function(){
     const pinInputDisplay = document.getElementById('pin-input-display');
     pinInputDisplay.value = generateRandomNum();
 });
+
+// Event Listener For Key Pad
+document.getElementById('key-pad').addEventListener('click', function(event) {
+    const number = event.target.innerText;
+    const numberInputDisplay = document.getElementById('number-input-display');
+
+    if(isNaN(number)) {
+        if(number === 'C') {
+            numberInputDisplay.value = '';
+        }
+    }
+    else {
+        const newNumber = numberInputDisplay.value + number;
+        numberInputDisplay.value = newNumber;
+    }
+});
+
+// Match Pin Event Listener Function
+function matchPin() {
+    console.log("I'm clicked!!");
+}
